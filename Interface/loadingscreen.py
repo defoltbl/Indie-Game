8b6158b3f0bc67ad30f9a1f47e0c5bb74f5d6d6e
Andrii Maksymenko <79446015+defoltbl@users.noreply.github.com>
2026-06-20 14:48:09 +0200

    Refactor loadingscreen.py for code clarity

--- Interface/loadingscreen.py
+++ Interface/loadingscreen.py
@@ -8,11 +8,11 @@ class LoadingScreen:
         self.font = pygame.font.Font(None, 36)
         self.text = self.font.render('Press Enter to Start', True, (255, 255, 255))
         self.text_rect = self.text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
-        self.background_image = pygame.image.load('../graphics/loadscreen/load.jpg').convert()  # Загрузка изображения
+        self.background_image = pygame.image.load('../graphics/loadscreen/load.jpg').convert() 
 
     def update(self, screen):
-        scaled_image = pygame.transform.scale(self.background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))  # Изменение размера изображения
-        screen.blit(scaled_image, (0, 0))  # Отображение измененного изображения на фоне
+        scaled_image = pygame.transform.scale(self.background_image, (SCREEN_WIDTH, SCREEN_HEIGHT)) 
+        screen.blit(scaled_image, (0, 0))  
         screen.blit(self.text, self.text_rect)
 
     def handle_events(self):
@@ -22,6 +22,6 @@ class LoadingScreen:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    return False  # Сигнал продолжить игру
-        return True  # Сигнал оставаться на экране загрузки
+                    return False 
+        return True 
     
