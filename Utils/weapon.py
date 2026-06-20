c5ec2bdeb82f501936a7799eee02e01eea29d3bc
Andrii Maksymenko <79446015+defoltbl@users.noreply.github.com>
2026-06-20 15:07:41 +0200

    Clean up comments in weapon.py
    Removed comments related to graphics loading and placement.

--- Utils/weapon.py
+++ Utils/weapon.py
@@ -7,12 +7,10 @@ class Weapon(pygame.sprite.Sprite):
         self.sprite_type = 'weapon'
         direction = player.status.split('_')[0]
 
-        # Загрузка графики
         full_path = f'../graphics/weapons/{player.weapon}/{direction}.png'
         self.image = pygame.image.load(full_path).convert_alpha()
         self.z = LAYERS['main']
 
-		# placement
         if direction == 'right':
              self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(0,16))
         elif direction == 'left': 
